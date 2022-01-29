a9edf0ec6494ceda09a06c46fd33bc93d38ee6ef
Artyu24 <arthurpejoiane@hotmail.fr>
2022-01-29 16:57:00 +0100

    Add Acceleration

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,36 +7,57 @@ public class GameManager : MonoBehaviour
     private static GameManager _instance = null;
     public static GameManager Instance { get => _instance; }
 
+    [Header("Vitesse des cubes")]
     [SerializeField] private float speed;
     public float GetSpeed { get => speed; }
 
+    [Header("Accélération")]
+    private float accel;
+    [SerializeField] private float timeBetweenAccel;
+    
+    [Header("Réduction du temps à chaque accel (<0.1f)")]
+    [SerializeField] private float timeMultiplierDecrease;
+    private float timeMultiplier;
+    public float GetTimeMultiplier { get { return timeMultiplier; } }
+
+    [Header("Spawn & Cubes")]
     [SerializeField] private GameObject leftSpawn, rightSpawn;
     [SerializeField] private GameObject blackCube, whiteCube;
 
+    [Header("Tableau des paternes")]
     [SerializeField] private PaternCube[] tabPatern;
 
     private bool isPaternPlay;
     public bool GetIsPaternPlay { get { return isPaternPlay; } set { isPaternPlay = value; } }
 
     private bool isWhiteSide;
+    private bool isAccelFinish;
+    private bool isScoreIncrease;
 
     private void Awake()
     {
         _instance = this;
+
+        timeMultiplier = 1;
+        accel = speed * 0.2f;
     }
 
     private void Update()
     {
+        if (!isAccelFinish && speed < accel * 15)
+            StartCoroutine(AccelGame());
+
+
+        if (!isScoreIncrease)
+            StartCoroutine(ScoreIncrease());
+
         if (!isPaternPlay)
-        {
             PlayRandomPatern();
-        }   
     }
 
     private void PlayRandomPatern()
     {
         int random = Random.Range(0, tabPatern.Length);
-        Debug.Log("Patern : " + random);
         tabPatern[random].ActivateCoroutine();
     }
 
@@ -85,6 +106,24 @@ public class GameManager : MonoBehaviour
         GameObject _blackCube = Instantiate(blackCube, spawnPosition.transform.position, Quaternion.identity);
         if (!isWhiteSide)
             _blackCube.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    private IEnumerator AccelGame()
+    {
+        isAccelFinish = true;
+        yield return new WaitForSeconds(timeBetweenAccel);
+        Debug.Log("Accel !");
+        speed += accel;
+        timeMultiplier -= timeMultiplierDecrease;
+        isAccelFinish = false;
+    }
+
+    private IEnumerator ScoreIncrease()
+    {
+        isScoreIncrease = true;
+        yield return new WaitForSeconds(1);
+        isScoreIncrease = false;
+
     }
 
     private GameObject[] BlackCubeTab()
