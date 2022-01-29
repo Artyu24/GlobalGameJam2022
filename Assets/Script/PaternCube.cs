2d4f2f93909328cd77731d069625585aaf266f13
Artyu24 <arthurpejoiane@hotmail.fr>
2022-01-29 13:16:31 +0100

    Better System Patern

--- Assets/Script/PaternCube.cs
+++ Assets/Script/PaternCube.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class PaternCube : MonoBehaviour
 {
-    [Header("Line 1")]
-    [SerializeField] private GameObject leftCube_1;
-    [SerializeField] private GameObject rightCube_1;
-    [SerializeField] private float timeBetweenThem_1;
-    [Header("Line 2")]
-    [SerializeField] private GameObject leftCube_2;
-    [SerializeField] private GameObject rightCube_2;
-    [SerializeField] private float timeBetweenThem_2;
-    [Header("Line 3")]
-    [SerializeField] private GameObject leftCube_3;
-    [SerializeField] private GameObject rightCube_3;
+    public List<PaternLayout> listPatern;
+
+    [System.Serializable]
+    public struct PaternLayout
+    {
+        public GameObject leftCube;
+        public GameObject rightCube;
+        public float timeBetweenThem;
+    }
 
     public void Update()
     {
@@ -26,13 +24,11 @@ public class PaternCube : MonoBehaviour
 
     public IEnumerator Patern()
     {
-        GameManager.Instance.SpawnCube("left", leftCube_1);
-        GameManager.Instance.SpawnCube("right", rightCube_1);
-        yield return new WaitForSeconds(timeBetweenThem_1 + .3f);
-        GameManager.Instance.SpawnCube("left", leftCube_2);
-        GameManager.Instance.SpawnCube("right", rightCube_2);
-        yield return new WaitForSeconds(timeBetweenThem_2 + .3f);
-        GameManager.Instance.SpawnCube("left", leftCube_3);
-        GameManager.Instance.SpawnCube("right", rightCube_3);
+        foreach (PaternLayout layout in listPatern)
+        {
+            GameManager.Instance.SpawnCube("left", layout.leftCube);
+            GameManager.Instance.SpawnCube("right", layout.rightCube);
+            yield return new WaitForSeconds(layout.timeBetweenThem + .3f);
+        }
     }
 }
