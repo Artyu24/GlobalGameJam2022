7462be6f65a6476315414e5c306c2b2d832c9363
Artyu24 <arthurpejoiane@hotmail.fr>
2022-01-29 12:53:22 +0100

    Add TestPaternButton

--- Assets/Script/PaternCube.cs
+++ Assets/Script/PaternCube.cs
@@ -16,14 +16,22 @@ public class PaternCube : MonoBehaviour
     [SerializeField] private GameObject leftCube_3;
     [SerializeField] private GameObject rightCube_3;
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(Patern());
+        }
+    }
+
     public IEnumerator Patern()
     {
         GameManager.Instance.SpawnCube("left", leftCube_1);
         GameManager.Instance.SpawnCube("right", rightCube_1);
-        yield return new WaitForSeconds(timeBetweenThem_1);
+        yield return new WaitForSeconds(timeBetweenThem_1 + .3f);
         GameManager.Instance.SpawnCube("left", leftCube_2);
         GameManager.Instance.SpawnCube("right", rightCube_2);
-        yield return new WaitForSeconds(timeBetweenThem_2);
+        yield return new WaitForSeconds(timeBetweenThem_2 + .3f);
         GameManager.Instance.SpawnCube("left", leftCube_3);
         GameManager.Instance.SpawnCube("right", rightCube_3);
     }
